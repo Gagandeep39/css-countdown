68484097abed2a2816e79b482ec308d9b035aa57
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-05-10 18:40:49 +0530

    Added JS for countdown toggle

---
+++ script.js
@@ -0,0 +1,25 @@
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.counter');
+const finalMessage = document.querySelector('.final');
+const replay = document.querySelector('#replay');
+
+runAnimation();
+
+function runAnimation() {
+  nums.forEach((num, index) => {
+    const nextToLast = nums.length - 1;
+
+    // Replace In animation with out animation class
+    num.addEventListener('animationend', (e) => {
+      if (e.animationName === 'goIn' && index !== nextToLast) {
+        num.classList.remove('in');
+        num.classList.add('out');
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in');
+      } else {
+        counter.classList.add('hide');
+        finalMessage.classList.add('show');
+      }
+    });
+  });
+}
